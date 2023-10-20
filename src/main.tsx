e25a4eb8459d920d9efe61ea833475e237206b8e
thiago-esteves <testeves.mao@gmail.com>
2023-10-20 19:28:52 -0300

    aula 01 parte 02

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Login } from './views/Login.tsx'
 
+import './assets/styles/global.scss';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Login />
